eb179291ef4d91de8b966b3411131614e55d7c27
Kentaro Miura <z.kentaro.miura@g-pc-00395351.intra.gree-office.net>
2017-01-27 18:34:18 +0900

    test code add

--- Test/ViewController.m
+++ Test/ViewController.m
@@ -17,6 +17,22 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
 
+    int count = 0;
+    if (count == 0)
+    {
+        NSLog(@"count %d",count);
+    }
+    else if(count == 1)
+    {
+        NSLog(@"count %d",count);
+    }
+    
+    UILabel *countLabel = [[UILabel alloc] initWithFrame:CGRectMake(100, 100, 100, 100)];
+    countLabel.backgroundColor = [UIColor yellowColor];
+    countLabel.textAlignment = NSTextAlignmentCenter;
+    countLabel.text = [NSString stringWithFormat:@"count %d",count];
+    [self.view addSubview:countLabel];
+
 }
 
 - (void)didReceiveMemoryWarning {
